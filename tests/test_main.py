ba5611a0c25e829f507b579ef250995d155fb169
Taylør Luttrell <tluttrellwilliams@gmail.com>
2026-02-15 16:11:21 -0600

    Removed Tests to Reduce Coverage

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,18 @@
 from src.main import factorial
 
 
-def test_factorial_zero() -> None:
-    assert factorial(0) == 1
-
-
-def test_factorial_one() -> None:
-    assert factorial(1) == 1
-
-
 def test_factorial_positive() -> None:
     assert factorial(5) == 120
     assert factorial(6) == 720
     assert factorial(10) == 3628800
+    
+
+# def test_factorial_zero() -> None:
+#     assert factorial(0) == 1
+
+
+# def test_factorial_one() -> None:
+#     assert factorial(1) == 1
 
 
 # def test_factorial_negative_raises() -> None:
