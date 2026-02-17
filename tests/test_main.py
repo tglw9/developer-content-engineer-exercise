66fd3daf5eb2ccd0340b2d5ce7aca4f7802cc1c6
Taylør Luttrell <tluttrellwilliams@gmail.com>
2026-02-16 18:16:01 -0600

    Enhanced Unit Test for Recursive Factorial

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,16 @@ from src.main import factorial
 
 
 class TestFactorial(unittest.TestCase):
-    def test_factorial_positive(self) -> None:
+    def test_factorial(self) -> None:
+        # recursive cases
         self.assertEqual(factorial(5), 120)
         self.assertEqual(factorial(6), 720)
         self.assertEqual(factorial(10), 3628800)
+
+        # base cases
+        self.assertEqual(factorial(0), 1)
+        self.assertEqual(factorial(1), 1)
+
+        # negative input (exception branch)
+        with self.assertRaises(ValueError):
+            factorial(-1)
